5c079d38036efb5d560394788ee168cbba17d3fc
JonathanAlejandro18 <jonarios182@gmail.com>
2024-08-22 10:57:06 -0600

    imagenes

--- srcphp/Models/productos_servicios.php
+++ srcphp/Models/productos_servicios.php
@@ -15,9 +15,8 @@ class productos_servicios extends Models
         $todoslosproductos = $producto->query("SELECT productos_servicios.ID_PRODUCTO AS ID, productos_servicios.NOMBRE, productos_servicios.DESCRIPCION,
                                                 productos_servicios.PRECIO, productos_servicios.STOCK, categoria_productos.NOMBRE AS CATEGORIA, productos_servicios.IMAGEN
                                                 FROM categoria_productos INNER JOIN productos_servicios ON categoria_productos.ID_CATEGORIA = productos_servicios.ID_CATEGORIA");
-
         $success = new Success($todoslosproductos);
-        return $success->send();
+        return $success->send();    
     }
 
     public function mostrarProductosAdmin(){
